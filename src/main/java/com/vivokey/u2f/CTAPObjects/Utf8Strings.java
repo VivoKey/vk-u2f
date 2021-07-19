60b59fca0571faee7804a38938637f183cae8488
Riley Gall <rileyg98@gmail.com>
2021-07-19 20:07:46 +1000

    Oops, forgot a file
    Utf8Strings not added.

---
+++ src/main/java/com/vivokey/u2f/CTAPObjects/Utf8Strings.java
@@ -0,0 +1,24 @@
+package com.vivokey.u2f.CTAPObjects;
+
+public class Utf8Strings {
+    public static final byte[] UTF8_UP = {'u', 'p'};
+    // Representation of "id" in UTF8
+    public static final byte[] UTF8_ID = {0x69, 0x64};
+    // Representation of "name" in UTF8
+    public static final byte[] UTF8_NAME = {0x6e, 0x61, 0x6d, 0x65};
+    // Representation of "displayName" in UTF8
+    public static final byte[] UTF8_DISPLAYNAME = {0x64, 0x69, 0x73, 0x70, 0x6c, 0x61, 0x79, 0x4e, 0x61, 0x6d, 0x65};
+    // Representation of "alg" in UTF8
+    public static final byte[] UTF8_ALG = {0x61, 0x6c, 0x67};
+    public static final byte[] UTF8_UV = {0x75, 0x76};
+    public static final byte[] UTF8_RK = {0x72, 0x6b};
+    public static final byte[] UTF8_TYPE = {'t', 'y', 'p', 'e'};
+    public static final byte[] UTF8_FMT = {'f', 'm', 't'};
+    public static final byte[] UTF8_AUTHDATA = {'a', 'u', 't', 'h', 'D', 'a', 't', 'a'};
+    public static final byte[] UTF8_PACKED = {'p', 'a', 'c', 'k', 'e', 'd'};
+    public static final byte[] UTF8_SIG = {'s', 'i', 'g'};
+    public static final byte[] UTF8_X5C = {'x', '5', 'c'};
+    public static final byte[] UTF8_ATTSTMT = {'a', 't', 't', 'S', 't', 'm', 't'};
+    public static final byte[] UTF8_PUBLIC_KEY = {'p', 'u', 'b', 'l', 'i', 'c', '-', 'k', 'e', 'y'};
+    
+}
