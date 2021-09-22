902ff9d372c1e1f9ec6bf6b83deec5eaa307b241
rileyg98 <rileyg98@users.noreply.github.com>
2021-09-23 00:16:40 +1000

    Fix how we read the map
    Was reading map stupidly, so reading it properly now

--- src/main/java/com/vivokey/u2f/HMACSecret.java
+++ src/main/java/com/vivokey/u2f/HMACSecret.java
@@ -11,9 +11,7 @@ public class HMACSecret {
     public byte[] encSalts;
     public byte[] auth;
 
-
     public HMACSecret(CBORDecoder dec) throws UserException {
-        
 
         // Start decoding the hmac-secret part of this
         short len = dec.readMajorType(CBORBase.TYPE_MAP);
@@ -27,49 +25,64 @@ public class HMACSecret {
                         UserException.throwIt(CTAP2.CTAP2_ERR_UNSUPPORTED_ALGORITHM);
                         break;
                     }
-                    // Tag 1: kty
-                    dec.readInt8();
-                    // Value, must be 2
-                    if (dec.readInt8() != (byte) 2) {
-                        UserException.throwIt(CTAP2.CTAP2_ERR_UNSUPPORTED_ALGORITHM);
-                        break;
-                    }
-                    // Tag 3: alg
-                    dec.readInt8();
-                    // Value, must be 24 (-25 is -1 - 24)
-                    if (dec.readInt8() != (byte) 24) {
-                        UserException.throwIt(CTAP2.CTAP2_ERR_UNSUPPORTED_ALGORITHM);
-                        break;
-                    }
-                    // Tag -1: curve
-                    dec.readInt8();
-                    // Value, must be 1
-                    if (dec.readInt8() != (byte) 1) {
-                        UserException.throwIt(CTAP2.CTAP2_ERR_UNSUPPORTED_ALGORITHM);
-                        break;
-                    }
-                    // Tag -2: X-coord
-                    dec.readInt8();
                     // Initialise w to make the pubkey out of
                     try {
                         w = JCSystem.makeTransientByteArray((short) 65, JCSystem.CLEAR_ON_RESET);
                     } catch (Exception e) {
                         w = new byte[65];
                     }
-                    w[0] = 0x04;
-                    // Read the X-coordinate in
-                    try {
-                    dec.readByteString(w, (short) 1);
-                    } catch (Exception e) {
-                        UserException.throwIt((byte)0x81);
-                    }
-                    // Tag -3: Y-coord
-                    dec.readInt8();
-                    // Read the Y-coordinate in
-                    try {
-                    dec.readByteString(w, (short) 33);
-                    } catch (Exception e) {
-                        UserException.throwIt((byte) 0x82);
+                    for (short j = 0; j < coseLen; j++) {
+                        short key = 0;
+                        if (dec.getMajorType() == CBORBase.TYPE_NEGATIVE_INTEGER) {
+                            key = (short) ((short) (-1) - dec.readInt8());
+                        } else if (dec.getMajorType() == CBORBase.TYPE_UNSIGNED_INTEGER) {
+                            key = dec.readInt8();
+                        } else {
+                            UserException.throwIt(CTAP2.CTAP2_ERR_INVALID_CBOR);
+                            break;
+                        }
+                        switch (key) {
+                            case 1:
+                                // Value should be 2
+                                if (dec.readInt8() != 2) {
+                                    UserException.throwIt(CTAP2.CTAP2_ERR_UNSUPPORTED_ALGORITHM);
+                                    break;
+                                }
+                                break;
+                            case 3:
+                                // Value, must be 24 (-25 is -1 - 24)
+                                if (dec.readInt8() != 24) {
+                                    UserException.throwIt(CTAP2.CTAP2_ERR_UNSUPPORTED_ALGORITHM);
+                                    break;
+                                }
+                                break;
+                            case -1:
+                                // Crv, should be 1
+                                if (dec.readInt8() != 1) {
+                                    UserException.throwIt(CTAP2.CTAP2_ERR_UNSUPPORTED_ALGORITHM);
+                                    break;
+                                }
+                                break;
+                            case -2:
+                                try {
+                                    dec.readByteString(w, (short) 1);
+                                } catch (Exception e) {
+                                    UserException.throwIt((byte) 0x81);
+                                    break;
+                                }
+                                break;
+                            case -3:
+                                try {
+                                    dec.readByteString(w, (short) 33);
+                                } catch (Exception e) {
+                                    UserException.throwIt((byte) 0x82);
+                                    break;
+                                }
+                                break;
+                            default:
+                                UserException.throwIt(CTAP2.CTAP2_ERR_INVALID_CBOR);
+                                break;
+                        }
                     }
                     break;
                 case 0x02:
@@ -78,15 +91,15 @@ public class HMACSecret {
                     byte[] tmp = new byte[64];
                     short len2 = 0;
                     try {
-                    len = dec.readByteString(tmp, (short) 0);
+                        len = dec.readByteString(tmp, (short) 0);
                     } catch (Exception e) {
                         UserException.throwIt((byte) 0x83);
                         break;
                     }
-                    if(len2 == 32) {
+                    if (len2 == 32) {
                         encSalts = new byte[32];
                         Util.arrayCopy(tmp, (short) 0, encSalts, (short) 0, (short) 32);
-                    } else if(len2 == 64) {
+                    } else if (len2 == 64) {
                         encSalts = new byte[64];
                         Util.arrayCopy(tmp, (short) 0, encSalts, (short) 0, (short) 64);
                     } else {
@@ -98,9 +111,9 @@ public class HMACSecret {
                     // Read it in
                     auth = new byte[16];
                     try {
-                    dec.readByteString(auth, (short) 0);
+                        dec.readByteString(auth, (short) 0);
                     } catch (UserException e) {
-                        UserException.throwIt((byte)0x80);
+                        UserException.throwIt((byte) 0x80);
                         break;
                     }
                     break;
