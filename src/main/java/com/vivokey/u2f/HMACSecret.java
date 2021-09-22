ab8fc18dbd2c296a7e593e7c4017f8a23495ee18
rileyg98 <rileyg98@users.noreply.github.com>
2021-09-23 01:47:36 +1000

    Update HMACSecret.java

--- src/main/java/com/vivokey/u2f/HMACSecret.java
+++ src/main/java/com/vivokey/u2f/HMACSecret.java
@@ -69,7 +69,7 @@ public class HMACSecret {
                     try {
                         dec.readByteString(w, (short) 33);
                     } catch (UserException e) {
-                        UserException.throwIt((byte) 0x72);
+                        UserException.throwIt(dec.getCurrentOffset());
                         break;
                     }
                     break;
